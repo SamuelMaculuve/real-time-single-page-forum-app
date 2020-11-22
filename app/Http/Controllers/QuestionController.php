9cd7cdb5704db83ac8d7c618919dda2a562c8caf
Samuel Maculuve <32066945+SamuelMaculuve@users.noreply.github.com>
2020-11-22 19:41:32 +0200

    Crud for Category

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -55,9 +55,10 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+        return response('update',Response::HTTP_ACCEPTED);
     }
-
+//
     /**
      * Remove the specified resource from storage.
      *
